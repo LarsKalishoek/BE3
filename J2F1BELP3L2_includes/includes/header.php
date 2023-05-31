5511cc9eb3e86f3322284e9a033911583f119b8b
LarsKalishoek1 <lars.kalishoek@student.divd.nl>
2023-05-31 15:56:03 +0200

    eindopdracht BE3 gemaakt

---
+++ J2F1BELP3L2_includes/includes/header.php
@@ -0,0 +1,25 @@
+<?php
+echo '
+        <header>
+            <h1>Eindopdracht</h1>
+            <nav>
+                <a href="index.php?subject=onderwerp1.php">
+                    <img src="images/mysql.jpg">
+                    <p>MySQL</p>
+                </a>
+                <a href="index.php?subject=onderwerp2.php">
+                    <img src="images/pdo.png">
+                    <p>PDO</p>
+                </a>
+                <a href="index.php?subject=onderwerp3.php">
+                    <img src="images/php.jpg">
+                    <p>PHP</p>
+                </a>
+                <a href="index.php?subject=onderwerp4.php">
+                    <img src="images/client_server.jpg">
+                    <p>Client Server</p>
+                </a>
+            </nav>
+        </header>
+    ';
+?>
--- variables.php
+++ variables.php
@@ -1,3 +1,5 @@
 <?php 
-
-$fruit = array("appel", "banaan","peer");
+$naam = "Lars Kalishoek";
+echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui veniam nobis quas earum ea amet pariatur velit? Adipisci odit, eaque quo earum molestias perferendis eos, corrupti quos maxime mollitia quas!";
+// $fruit = array("appel", "banaan","peer");
+echo "<img src= https://m.media-amazon.com/images/I/81EmlXBfPuL._AC_SX425_.jpg>";
